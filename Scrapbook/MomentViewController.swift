b2733bc774190bddbfc4c6d5f0f217937597d005
alan-yilun-li <alan.yilun.li@gmail.com>
2016-12-13 23:34:01 -0500

    Made the save button auto-enabled when editing
    - Checked what the presentingViewController is and altered the save
    button accordingly

--- Scrapbook/MomentViewController.swift
+++ Scrapbook/MomentViewController.swift
@@ -31,12 +31,23 @@ class MomentViewController: UIViewController, UITextFieldDelegate, UIImagePicker
         // Will be notified when keyboard shows up (for scrolling)
         registerForKeyboardNotifications()
         
-        // Checking for valid title and photo to enable save button
-        checkValidMomentName()
-        checkValidPhoto()
-        
-        
         self.scrollView.isScrollEnabled = true
+        
+        
+        // Checking if the screen was presented by the add button
+        let isPresentingInAddMomentMode = presentingViewController is UINavigationController
+        
+        if !isPresentingInAddMomentMode{
+            self.saveButton.isEnabled = true
+        }
+        
+        else {
+            
+            // Checking for valid title and photo to enable save button
+            checkValidMomentName()
+            checkValidPhoto()
+        }
+        
         
         // Caption Border
         captionTextView.layer.cornerRadius = 10
@@ -82,11 +93,11 @@ class MomentViewController: UIViewController, UITextFieldDelegate, UIImagePicker
         
         let text = nameTextField.text ?? ""
         saveButton.isEnabled = !text.isEmpty
-        
+        /*
         if (moment?.name) != nil {
             print("save should be working")
             saveButton.isEnabled = true
-        }
+        } */
     }
     
     func checkValidPhoto() {
