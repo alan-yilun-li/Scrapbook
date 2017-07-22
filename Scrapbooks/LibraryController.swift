23683a9459d17df0cff74d0c484e6e8afba38616
alan.yilun.li@gmail.com <alan.li@telus.com>
2017-07-21 21:33:27 -0400

    Reorganized files

--- Scrapbooks/LibraryController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LibraryController.swift
-//  Scrapbook
-//
-//  Created by Dev User on 2017-07-20.
-//  Copyright © 2017 Alan Li. All rights reserved.
-//
-
-import Foundation
--- Scrapbooks/Models.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Models.swift
-//  Scrapbook
-//
-//  Created by Alan Li on 2017-06-11.
-//  Copyright © 2017 Alan Li. All rights reserved.
-//
-
-import Foundation
