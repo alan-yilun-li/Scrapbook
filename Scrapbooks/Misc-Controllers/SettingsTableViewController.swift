fafa816af9a5bbdcc24af3eaf9bec7fa7e9f3f65
Alan Li <alan.yilun.li@gmail.com>
2017-09-23 02:41:37 -0400

    Added storyboard UI

---
+++ Scrapbooks/Misc-Controllers/SettingsTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsTableViewController.swift
+//  Scrapbooks
+//
+//  Created by Alan Li on 2017-09-23.
+//  Copyright © 2017 Alan Li. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
